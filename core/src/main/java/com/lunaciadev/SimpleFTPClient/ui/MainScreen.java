e556c16acdab4fc46003159f15d2da8166ff3942
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-03-05 02:55:56 +0000

    feat: main screen tabular layout

--- core/src/main/java/com/lunaciadev/SimpleFTPClient/ui/MainScreen.java
+++ core/src/main/java/com/lunaciadev/SimpleFTPClient/ui/MainScreen.java
@@ -1,17 +1,51 @@
 package com.lunaciadev.SimpleFTPClient.ui;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.lunaciadev.SimpleFTPClient.data.DataPackage;
 
 public class MainScreen implements Screen {
     private DataPackage dataPackage;
-
+    private Table rootTable;
+    private Stage stage;
     private Skin skin;
 
     public MainScreen(DataPackage dataPackage) {
         this.dataPackage = dataPackage;
         this.skin = dataPackage.getSkin();
+        this.stage = new Stage(new ScreenViewport());
+        setLayout();
+    }
+
+    private void setLayout() {
+        rootTable = new Table();
+        rootTable.setFillParent(true);
+        stage.addActor(rootTable);
+
+        HorizontalGroup mainWidgetGroup = new HorizontalGroup();
+        VerticalGroup serverFSView = new VerticalGroup();
+        VerticalGroup controlGroup = new VerticalGroup();
+        VerticalGroup localFSView = new VerticalGroup();
+
+        mainWidgetGroup.addActor(serverFSView);
+        mainWidgetGroup.addActor(controlGroup);
+        mainWidgetGroup.addActor(localFSView);
+
+        rootTable.add(mainWidgetGroup)
+                .expandX().fill();
+
+        HorizontalGroup statusGroup = new HorizontalGroup();
+
+        rootTable.row();
+        rootTable.add(statusGroup)
+                .expandX().fill();
     }
 
     @Override
@@ -22,8 +56,9 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'render'");
+        ScreenUtils.clear(Color.LIGHT_GRAY);
+        stage.act();
+        stage.draw();
     }
 
     @Override
